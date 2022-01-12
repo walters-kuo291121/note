40ee2cdd4698d131f86c5c1036e82be6e61cc5f0
walters <walters.kuo66@gmail.com>
2022-01-12 09:29:17 +0800

    add linux command, daemon, simple http server

---
+++ linux/SimpleHTTPServer.py
@@ -0,0 +1,12 @@
+#python -m http.server 8000 --bind 127.0.0.1
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
